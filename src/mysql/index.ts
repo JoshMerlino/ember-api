45ad502e5c426568c3ee936c96a9b1bca1392170
Josh Merlino <joshua.merlino@gmail.com>
2022-11-27 04:58:05 +0000

    automaticly configure database

--- src/mysql/index.ts
+++ src/mysql/index.ts
@@ -1,5 +1,7 @@
 import chalk from "chalk";
+import { readFile } from "fs/promises";
 import mysql from "mysql2";
+import path from "path";
 import credentials from "./credentials";
 
 export const connection = mysql.createConnection(credentials);
@@ -9,6 +11,25 @@ connection.connect((err) => {
 		console.error("Could not connect to MySQL server.", err);
 	} else {
 		console.info(chalk.cyan("SQL"), "Connected to MySQL server.");
+
+		(async function() {
+
+			// Get number of tables
+			const tables = await query("SHOW TABLES");
+			if (tables.length === 0) {
+				console.info(chalk.cyan("SQL"), "No tables found, Starting upload.");
+				const template = await readFile(path.resolve("src/mysql/template.sql"), "utf8");
+				for (let line of template.split(";")) {
+					if (line.trim() !== "") {
+						line = line.replace(/\s+|\n|\t/g, " ") + ";";
+						await query(line);
+					}
+				}
+				console.info(chalk.cyan("SQL"), "Upload complete.");
+			}
+
+		}());
+
 	}
 });
 
---
+++ src/mysql/template.sql
@@ -0,0 +1,59 @@
+CREATE TABLE `users`(
+    `id` BIGINT UNSIGNED NOT NULL,
+    `username` TEXT NOT NULL,
+    `email` TEXT NOT NULL,
+    `passwd_md5` TEXT NOT NULL,
+    `passwd_length` INT UNSIGNED NOT NULL,
+    `passwd_changed_ms` BIGINT UNSIGNED NOT NULL,
+    `created_ms` BIGINT NOT NULL,
+    `roles` TEXT NOT NULL,
+    `administrator` TINYINT NOT NULL
+);
+
+ALTER TABLE
+    `users` ADD UNIQUE `users_id_unique`(`id`);
+	
+CREATE TABLE `sessions`(
+    `id` BIGINT UNSIGNED NOT NULL,
+    `session_id` TEXT NOT NULL,
+    `user` BIGINT UNSIGNED NOT NULL,
+    `md5` TEXT NOT NULL,
+    `created_ms` BIGINT UNSIGNED NOT NULL,
+    `last_used_ms` BIGINT UNSIGNED NOT NULL,
+    `user_agent` TEXT NOT NULL,
+    `ip_address` TEXT NOT NULL
+);
+
+ALTER TABLE
+    `sessions` ADD UNIQUE `sessions_id_unique`(`id`);
+	
+CREATE TABLE `mfa`(
+    `id` BIGINT UNSIGNED NOT NULL,
+    `user` BIGINT UNSIGNED NOT NULL,
+    `secret` TEXT NOT NULL,
+    `pending` TINYINT NOT NULL
+);
+
+ALTER TABLE
+    `mfa` ADD UNIQUE `mfa_id_unique`(`id`);
+	
+CREATE TABLE `sso`(
+    `id` BIGINT UNSIGNED NOT NULL,
+    `user` BIGINT UNSIGNED NOT NULL,
+    `ssokey` TEXT NOT NULL,
+    `expires_after` BIGINT NOT NULL
+);
+
+ALTER TABLE
+    `sso` ADD UNIQUE `sso_id_unique`(`id`);
+	
+CREATE TABLE `roles`(
+    `id` BIGINT UNSIGNED NOT NULL,
+    `name` TEXT NOT NULL,
+    `color` INT NOT NULL,
+    `flags` INT NOT NULL
+);
+
+ALTER TABLE
+    `roles` ADD UNIQUE `roles_id_unique`(`id`);
+	
