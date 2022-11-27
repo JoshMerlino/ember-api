d1efeaacf99eda73df6c5ce8c5fd1cfba104b44e
Josh Merlino <joshua.merlino@gmail.com>
2022-11-27 18:09:36 +0000

    use mysql pool

--- src/mysql/index.ts
+++ src/mysql/index.ts
@@ -4,39 +4,63 @@ import mysql from "mysql2";
 import path from "path";
 import credentials from "./credentials";
 
-export const connection = mysql.createConnection(credentials);
+// Create connection pool
+const pool = mysql.createPool({
+	...credentials,
+	connectionLimit: 5
+});
 
-connection.connect((err) => {
-	if (err) {
-		console.error("Could not connect to MySQL server.", err);
-	} else {
-		console.info(chalk.cyan("SQL"), "Connected to MySQL server.");
+(function connect() {
 
-		(async function() {
+	// Connect to database
+	pool.getConnection((err, connection) => {
 
-			// Get number of tables
-			const tables = await query("SHOW TABLES");
-			if (tables.length === 0) {
-				console.info(chalk.cyan("SQL"), "No tables found, Starting upload.");
-				const template = await readFile(path.resolve("src/mysql/template.sql"), "utf8");
-				for (let line of template.split(";")) {
-					if (line.trim() !== "") {
-						line = line.replace(/\s+|\n|\t/g, " ") + ";";
-						await query(line);
-					}
-				}
-				console.info(chalk.cyan("SQL"), "Upload complete.");
+		// Handle error
+		if (err) {
+			console.error(chalk.cyan("SQL"), chalk.redBright("Error connecting to database: " + err.message));
+			if (process.env.DEVELOPMENT) console.error(err.stack);
+			return setTimeout(connect);
+		}
+
+		// Log connection
+		console.info(chalk.cyan("SQL"), "Connected to database as ID:", chalk.cyan(connection.threadId));
+
+		// Release connection
+		connection.release();
+
+	});
+
+}());
+
+// Before process exit
+process.on("beforeExit", () => {
+	pool.end();
+	console.info(chalk.cyan("SQL"), "Disconnected from database");
+});
+
+// Initialize database
+pool.on("connection", async function(){
+
+	// Get number of tables
+	const tables = await query("SHOW TABLES");
+	if (tables.length === 0) {
+		console.info(chalk.cyan("SQL"), "No tables found, Starting upload.");
+		const template = await readFile(path.resolve("src/mysql/template.sql"), "utf8");
+		for (let line of template.split(";")) {
+			if (line.trim() !== "") {
+				line = line.replace(/\s+|\n|\t/g, " ") + ";";
+				await query(line);
 			}
-
-		}());
-
+		}
+		console.info(chalk.cyan("SQL"), "Upload complete.");
 	}
+
 });
 
 export function query<T = Record<string, unknown>>(query: string): Promise<T[]> {
 	return new Promise((resolve, reject) => {
 		if (process.env.DEVELOPMENT) console.info(chalk.cyan("SQL"), chalk.green("QUERY"), query);
-		connection.query(query, function(error, data) {
+		pool.query(query, function(error, data) {
 			if (error !== null) return reject(error);
 			resolve(<T[]>data);
 		});
