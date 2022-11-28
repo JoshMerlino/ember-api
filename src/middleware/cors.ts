ce6e77dd7cb1a529acf7afb3a4d959d1782619f9
Josh Merlino <joshua.merlino@gmail.com>
2022-11-28 21:49:09 +0000

    update cors

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 // Export middleware
 export default cors({
 	origin: "*",
-	methods: [ "GET", "POST", "PUT", "DELETE", "PATCH" ]
-
+	methods: [ "GET", "POST", "PUT", "DELETE", "PATCH" ],
+	credentials: true,
+	preflightContinue: true
 });
