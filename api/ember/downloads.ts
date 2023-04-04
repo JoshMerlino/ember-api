882c9c72bdb70912771cbb67542db8d8b2bb9171
Josh Merlino <joshua.merlino@gmail.com>
2023-04-04 03:15:50 +0000

    better semver comparison

--- api/ember/downloads.ts
+++ api/ember/downloads.ts
@@ -9,29 +9,49 @@ const client = new Octokit({
 
 export default async function api(req: Request, res: Response): Promise<void | Response> {
 
-	// list all files in repo EmberVPN/releases
-	const win32 = await client.rest.repos.getContent({
-		owner: "EmberVPN",
-		repo: "releases",
-		path: "win32"
-	}).then(a => Array.from(a.data as RepoFile[])
-		.map(({ name }) => {
-			const version = name
-				.split(/\W/)
-				.map(a => parseInt(a))
-				.filter(a => !isNaN(a))
-				.join(".");
-			return {
-				name,
-				download_url: `https://media.githubusercontent.com/media/EmberVPN/releases/master/win32/embervpn-client-${ version }-win32-setup.exe`,
-				version
-			};
-		}));
-
 	interface RepoFile {
 		name: string;
 		download_url: string;
+		sha: string;
 	}
+
+	// Get version from file name
+	const v = (name: string) => name
+		.split(/\W/)
+		.map(a => parseInt(a))
+		.filter(a => !isNaN(a))
+		.join(".");
+	
+	// list all files in repo EmberVPN/releases
+	const win32 = await Promise.all(await client.rest.repos.getContent({
+		owner: "EmberVPN",
+		repo: "releases",
+		path: "win32"
+	}).then(async a => Array.from(a.data as RepoFile[])
+
+		// sort by version
+		.sort((a, b) => {
+			const aVersion = v(a.name);
+			const bVersion = v(b.name);
+			if (aVersion === bVersion) return 0;
+			const aParts = aVersion.split(".");
+			const bParts = bVersion.split(".");
+			for (let i = 0; i < Math.max(aParts.length, bParts.length); i++) {
+				const aPart = parseInt(aParts[i]);
+				const bPart = parseInt(bParts[i]);
+				if (aPart === bPart) continue;
+				return aPart > bPart ? 1 : -1;
+			}
+			return 0;
+		})
+		
+		// Map to file info
+		.map(async file => ({
+			name: file.name,
+			sha: file.sha,
+			version: v(file.name),
+			download_url: `https://media.githubusercontent.com/media/EmberVPN/releases/master/win32/${ file.name }`,
+		}))));
 
 	res.json({
 		success: true,
