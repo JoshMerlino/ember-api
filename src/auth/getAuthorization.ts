5f8bed2bdc7e0b8809487eccdb2a220026c67142
Josh Merlino <joshua.merlino@gmail.com>
2023-04-11 01:28:45 +0000

    add more ways to get authorized

--- src/auth/getAuthorization.ts
+++ src/auth/getAuthorization.ts
@@ -2,7 +2,10 @@ import { Request } from "express";
 
 export default function getAuthorization(req: Request): string | undefined {
 	if (req.header("authorization")) return req.header("authorization");
+	if (req.header("session_id")) return req.header("session_id");
 	if (typeof req.query.authorization === "string") return req.query.authorization;
+	if (typeof req.query.session_id === "string") return req.query.session_id;
+	if (req.cookies.authorization) return req.cookies.authorization;
 	if (req.cookies.session_id) return req.cookies.session_id;
 	return;
 }
