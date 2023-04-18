20a0881715da88ca593b64a5b63349809c16727a
Josh Merlino <joshua.merlino@gmail.com>
2023-04-18 00:50:04 +0000

    use nullish

--- src/ember/isAllowed.ts
+++ src/ember/isAllowed.ts
@@ -4,9 +4,9 @@ import User from "../auth/User";
 
 export async function isAllowed(server: Ember.Server, user: User<Auth.Meta>): Promise<false | Ember.Server> {
 	const accessMap: Record<string, string[]> = JSON.parse(await readFile(resolve("./userdata/accessMap.json"), "utf8"));
-	if (accessMap[user.id].includes(server.hash)) return server;
-	if (accessMap[user.id].includes(server.ip)) return server;
-	if (accessMap[user.id].includes(server.hash)) return server;
-	if (accessMap[user.id].includes("*")) return server;
+	if (accessMap[user.id]?.includes(server.hash)) return server;
+	if (accessMap[user.id]?.includes(server.ip)) return server;
+	if (accessMap[user.id]?.includes(server.hash)) return server;
+	if (accessMap[user.id]?.includes("*")) return server;
 	return false;
 }
