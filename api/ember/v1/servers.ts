0be597ccfe0addb69fb5eecc57520962e30f6e24
Josh Merlino <joshua.merlino@gmail.com>
2023-05-02 02:38:11 +0000

    update again

--- api/ember/v1/servers.ts
+++ api/ember/v1/servers.ts
@@ -1,26 +1,10 @@
 import { Request, Response } from "express";
 import { readFile } from "fs/promises";
-import { Socket } from "net";
 import { resolve } from "path";
 import User from "../../../src/auth/User";
 import getAuthorization from "../../../src/auth/getAuthorization";
 import { isAllowed } from "../../../src/ember/isAllowed";
 import rejectRequest from "../../../src/util/rejectRequest";
-
-// Helper function to ping a server
-export function ping(server: Ember.Server, timeout = 2000) {
-	return new Promise<number | false>(resolve => {
-		const start = Date.now();
-		const socket = new Socket;
-		socket.setTimeout(timeout);
-		socket.connect(parseInt(server.port), server.ip, () => {
-			socket.end();
-			resolve(Date.now() - start);
-		});
-		socket.on("error", () => resolve(false));
-		socket.on("timeout", () => resolve(false));
-	});
-}
 
 // Create cache
 const servers: Record<string, Ember.Server> = {};
@@ -30,11 +14,6 @@ setInterval(async function cache() {
 	Object.values(JSON.parse(await readFile(resolve("./userdata/servers.json"), "utf8")) as Record<string, Ember.Server>)
 		.map(server => servers[server.hash] = server);
 	
-	// Ping servers
-	await Promise.all(Object.values(servers).map(async server => {
-		if (servers && !servers[server.hash].ping) servers[server.hash].ping = await ping(server);
-	}));
-
 }, 1000);
 
 export const route = "ember/servers";
@@ -61,7 +40,6 @@ export default async function api(req: Request, res: Response): Promise<void | R
 	return res.json({
 		success: true,
 		servers: Object.values(usersServers)
-			.filter(server => server.ping)
 			.reduce((obj, server) => {
 				obj[server.hash] = server;
 				return obj;
