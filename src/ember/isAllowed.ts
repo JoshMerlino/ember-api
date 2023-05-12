599aa7155f38c3f653bb32b7f5a82b7ccfa76aef
Josh Merlino <joshua.merlino@gmail.com>
2023-05-12 06:16:06 +0000

    only allowing users with subscriptions to use the client

--- src/ember/isAllowed.ts
+++ src/ember/isAllowed.ts
@@ -1,12 +1,24 @@
 import User from "../auth/User";
+import { stripe } from "../stripe";
 
-export async function isAllowed(server: Ember.Server, _user: User): Promise<false | Ember.Server> {
-	return server;
+export async function isAllowed(server: Ember.Server, user: User): Promise<false | Ember.Server> {
 
-	// const accessMap: Record<string, string[]> = JSON.parse(await readFile(resolve("./userdata/accessMap.json"), "utf8"));
-	// if (accessMap[user.id]?.includes(server.hash)) return server;
-	// if (accessMap[user.id]?.includes(server.ip)) return server;
-	// if (accessMap[user.id]?.includes(server.hash)) return server;
-	// if (accessMap[user.id]?.includes("*")) return server;
-	// return false;
+	// Get the users customer id
+	const customer = await user.getCustomer().then(customer => customer.id);
+
+	// Get the users subscription
+	const subscriptions = await stripe.subscriptions.list({ customer, expand: [ "data.default_payment_method", "data.plan.product" ]})
+		.then(({ data }) => data as unknown as Ember.Subscription[]);
+	
+	// Get the active subscription
+	const active = subscriptions
+		.filter(subscription => subscription.cancel_at_period_end === false)
+		.filter(subscription => subscription.status === "active");
+
+	// If the user has an active subscription
+	if (active.length > 0) return server;
+
+	// No plan no server
+	return false;
+	
 }
