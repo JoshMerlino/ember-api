71fe5298703279ecd229bf95fe0310c34370a6cf
Josh Merlino <joshua.merlino@gmail.com>
2023-07-04 19:37:23 -0400

    remove old smtp

--- src/smtp.ts
+++
@@ -1,9 +0,0 @@
-import nodemailer from "nodemailer";
-
-export default nodemailer.createTransport({
-	service: "gmail",
-	auth: {
-		user: process.env.SMTP_USER,
-		pass: process.env.SMTP_PASS
-	}
-});
