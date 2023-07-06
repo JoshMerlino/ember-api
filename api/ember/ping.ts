77ba454b58229d92c48904930775c726857287d6
Josh Merlino <joshua.merlino@gmail.com>
2023-07-06 12:13:41 -0400

    add ping to api server

---
+++ api/ember/ping.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import { sql } from "../../src/mysql";
+
+// Cache all the timers
+const timers: Record<string, NodeJS.Timer> = {};
+const strikes: Record<string, number> = {};
+
+// Select all servers and start a timer for them
+(async function start_timers() {
+	setTimeout(start_timers, 30000);
+	await sql<MySQLData.Server[]>`SELECT * FROM servers`.then(servers => servers.map(function(server) {
+		
+		// Make sure its not already running
+		if (timers[server.uuid]) return;
+
+		// Start timer
+		strikes[server.uuid] = 0;
+		timers[server.uuid] = setInterval(async function() {
+			
+			// Add strike
+			strikes[server.uuid]++;
+
+			// If strikes 3 times, remove server
+			if (strikes[server.uuid] >= 3) {
+				clearInterval(timers[server.uuid]);
+				delete timers[server.uuid];
+				delete strikes[server.uuid];
+				await sql`DELETE FROM servers WHERE uuid = ${ server.uuid }`;
+			}
+			
+		}, 30000);
+
+	}));
+}());
+
+export const route = "ember/ping";
+export default async function api(req: Request, res: Response): Promise<void | Response> {
+
+	const body = { ...req.query, ...req.body };
+	const { hostname } = body;
+
+	res.json({ success: true, hostname });
+
+}
